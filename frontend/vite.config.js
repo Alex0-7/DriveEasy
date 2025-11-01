d0c3d609f411b205c24271fc3873c306f7668e57
Alex0-7 <allenalexthomas7@gmail.com>
2025-11-01 22:24:30 +0530

    updated many

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/DriveEasy",
-  server: {
-    port: 3000,
-  },
-
+  base: './', // Use relative paths
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
